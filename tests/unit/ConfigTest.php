72b45bb9a2ef0ac35eac201a941e3fdc999e98df
Mike Hickey <mike@27cubes.com>
2013-12-16 11:25:31 -0500

    Started stubbing out unit test files

---
+++ tests/unit/ConfigTest.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * Tests the Sptk\DbConnectionPool\Manager class
+ *
+ * PHP version 5.3
+ *
+ * @vendor     27Cubes
+ * @package    DbSmart2
+ * @subpackage DmSmart2Test
+ * @author     27 Cubes <info@27cubes.net>
+ * @since      %NEXT_VERSION%
+ */
+
+namespace 27Cubes\DbSmart2;
+
+class ConfigTest extends \PHPUnit_Framework_TestCase
+{
+	/**
+	 * Useless test to start out
+	 */
+	public function testGetInstance()
+	{
+		$obj = \27Cubes\DbSmart2\Config();
+		$this->assertInstanceOf('\27Cubes\DbSmart2\Config', $obj);
+	}
+}
